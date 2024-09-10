f7eb3f0e4f70cae1f3b0611d68a5bb0f159c2cad
xinyu-24 <a1810129@student.adelaide.edu.au>
2024-09-10 19:23:46 +0930

    Instructor.h

---
+++ Instructor.h
@@ -0,0 +1,11 @@
+#ifndef INSTRUCTOR_H
+#define INSTRUCTOR_H
+
+#include "Person.h"
+
+class Instructor : public Person {
+public:
+    Instructor(const std::string& name);
+};
+
+#endif // INSTRUCTOR_H
